e8a3c267b32370b33201bf41da9f86172f691935
ADMIN <thutrathao@gmail.com>
2022-04-25 00:51:31 +0700

    Completed note by file

--- app/src/main/java/com/example/allnotes/FileNoteActivity.java
+++ app/src/main/java/com/example/allnotes/FileNoteActivity.java
@@ -2,13 +2,18 @@ package com.example.allnotes;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -17,20 +22,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileNoteActivity extends AppCompatActivity {
+public class FileNoteActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     ListView listView;
     DatabaseReference databaseReference;
     List<putPDF> uploadFile;
     FloatingActionButton btnCreateFile;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    FirebaseAuth mAuth;
+
     putPDF putPDF;
 
 
@@ -40,11 +53,24 @@ public class FileNoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_file_note);
         listView = findViewById(R.id.listFile);
         btnCreateFile = findViewById(R.id.createFileFab);
+        drawerLayout = findViewById(R.id.draw_layout_file);
+        navigationView = findViewById(R.id.nav_view_file);
+        toolbar = findViewById(R.id.toolbar_file);
+        mAuth = FirebaseAuth.getInstance();
+
         uploadFile = new ArrayList<>();
 
         Intent intent = getIntent();
         String key = intent.getStringExtra("key");
         databaseReference = FirebaseDatabase.getInstance().getReference("upLoadPDF");
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_home);
         //databaseReference.orderByKey().equalTo(key);
         retrieveFile();
 
@@ -129,5 +155,32 @@ public class FileNoteActivity extends AppCompatActivity {
 
             }
         });
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.nav_home:
+                startActivity(new Intent(FileNoteActivity.this, ContentMain.class));
+                break;
+            case R.id.nav_draw:
+                startActivity(new Intent(FileNoteActivity.this, DrawActivity.class));
+                break;
+            case R.id.nav_image:
+                startActivity(new Intent(FileNoteActivity.this, ImageNoteActivity.class));
+                break;
+            case R.id.nav_file:
+                Intent intent = new Intent(FileNoteActivity.this, FileNoteActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_text:
+                startActivity(new Intent(FileNoteActivity.this,NoteTextActivity.class));
+                break;
+            case R.id.nav_logout:
+                mAuth.signOut();
+                finish();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
