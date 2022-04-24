49a73cc3c6b68122775ede0301ab8aa58262c29a
ADMIN <thutrathao@gmail.com>
2022-04-25 00:35:55 +0700

    Note by upload image

--- app/src/main/java/com/example/allnotes/ImageNoteActivity.java
+++ app/src/main/java/com/example/allnotes/ImageNoteActivity.java
@@ -1,14 +1,21 @@
 package com.example.allnotes;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,12 +24,16 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class ImageNoteActivity extends AppCompatActivity {
+public class ImageNoteActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private RecyclerView listImage;
     private ArrayList<ImageModel> list;
     private ImageAdapter adapter;
     DatabaseReference databaseReference;
     FloatingActionButton btnCreateImageNote;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    FirebaseAuth mAuth;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +41,19 @@ public class ImageNoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_image_note);
         listImage = findViewById(R.id.listImage);
         databaseReference = FirebaseDatabase.getInstance().getReference("Image");
-
+        mAuth = FirebaseAuth.getInstance();
         btnCreateImageNote = findViewById(R.id.createImageNote);
+        drawerLayout = findViewById(R.id.draw_layout_image);
+        navigationView = findViewById(R.id.nav_view_image);
+        toolbar = findViewById(R.id.toolbar_image);
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_home);
         listImage.setHasFixedSize(true);
         listImage.setLayoutManager(new LinearLayoutManager(this));
         list = new ArrayList<>();
@@ -73,5 +95,32 @@ public class ImageNoteActivity extends AppCompatActivity {
 
             }
         });
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.nav_home:
+                startActivity(new Intent(ImageNoteActivity.this, ContentMain.class));
+                break;
+            case R.id.nav_draw:
+                startActivity(new Intent(ImageNoteActivity.this, DrawActivity.class));
+                break;
+            case R.id.nav_image:
+                startActivity(new Intent(ImageNoteActivity.this, ImageNoteActivity.class));
+                break;
+            case R.id.nav_file:
+                Intent intent = new Intent(ImageNoteActivity.this, FileNoteActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_text:
+                startActivity(new Intent(ImageNoteActivity.this,NoteTextActivity.class));
+                break;
+            case R.id.nav_logout:
+                mAuth.signOut();
+                finish();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
